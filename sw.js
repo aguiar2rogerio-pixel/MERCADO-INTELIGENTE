2f6d7bb18085fa68a6d89bb75f2632f4de7bf6fb
aguiar2rogerio-pixel <aguiar2.rogerio@gmail.com>
2026-06-09 12:44:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,39 @@
-const CACHE_NAME = 'shopping-list-v6';
+const CACHE_NAME = 'smart-shopping-v2'; // Mudamos para v2 para forçar a limpeza do cache antigo
 const ASSETS = [
-  'https://aguiar2rogerio-pixel.github.io/MERCADO-INTELIGENTE/',
-  'https://aguiar2rogerio-pixel.github.io/MERCADO-INTELIGENTE/index.html',
-  'https://aguiar2rogerio-pixel.github.io/MERCADO-INTELIGENTE/manifest.json',
-  'https://aguiar2rogerio-pixel.github.io/MERCADO-INTELIGENTE/icon-192x192.png',
-  'https://aguiar2rogerio-pixel.github.io/MERCADO-INTELIGENTE/icon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
+// Instala o Service Worker e guarda os arquivos novos no cache
 self.addEventListener('install', (e) => {
-    self.skipWaiting();
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-    );
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting()) // Força o SW novo a se tornar ativo imediatamente
+  );
 });
 
+// Limpa os caches antigos (deleta a "versão velha" do aplicativo)
 self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
-        ))
-    );
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Assume o controle da página na hora
+  );
 });
 
+// Responde com o cache, mas busca na rede se houver internet
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
